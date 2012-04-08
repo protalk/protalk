d9593a3454761be727a0f9eeb8f873fd0790fd2a
Kim Rowan <rowan02@unknown-00-25-00-44-3a-04.home>
2012-04-08 15:58:06 +0100

    Added doc/db/seed_data.sql file to protalk root directory. Seeds database with sample Page records. Add to db using mysql -u root -p protalk < /path/to/protalk/doc/db/seed_data.sql

---
+++ doc/db/seed_data.sql
@@ -0,0 +1,10 @@
+/*
+* Use this file to load in some default data.
+*/
+
+
+/* Load sample pages */
+insert into `Page` (url,pagetitle,title,content) values ('contact','Contact Details','ProTalk Contact Details','Some dummy text for contact page');
+insert into `Page` (url,pagetitle,title,content) values ('contribute','How to Contribute','Contribute to ProTalk','Some dummy text for contribute page');
+insert into `Page` (url,pagetitle,title,content) values ('about','About ProTalk','About ProTalk','Some dummy text for about page');	
+
