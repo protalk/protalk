e3f6d97e30103da402ba563196937ee00f0a3a8f
Kim Rowan <rowan02@unknown-00-25-00-44-3a-04.home>
2012-04-12 11:11:39 +0100

    Completed HTML and basic functionality for Contact/Contribute/About pages, including pulling/displaying values from db. Changed YUI version in base.twig.html template to pull latest release from YUI CDN (3.5.0 instead of 3.4.1). Speaker photos now display pop-up "Speaker Biography" modal panel when clicked.  New values added to seed_data.sql to support current work.

--- doc/db/seed_data.sql
+++ doc/db/seed_data.sql
@@ -10,14 +10,30 @@
 
 DELETE FROM `Page`;
 
-INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contact','Contact Details','ProTalk Contact Details','Some dummy text for contact page');
-INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contribute','How to Contribute','Contribute to ProTalk','Some dummy text for contribute page');
-INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('about','About ProTalk','About ProTalk','Some dummy text for about page');
+INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contact','Contact Us','CONTACT','Some dummy text for contact page');
+INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contribute','How to Contribute','CONTRIBUTE','Some dummy text for contribute page');
+INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('about','About Us','ABOUT','Some dummy text for about page');
 
 
 /* Delete all Tag records then re-load sample data */
 
 DELETE FROM `Tag`;
 
-INSERT INTO `Tag` (name) VALUES ('PHP'),('Quality Assurance'),('MySQL'),('Strings'),('Arrays'),('PHPUnit'),('Refactoring'),('Tools'), ('Build Process'),('Deployment'), ('Integration'), ('DocBlox'),('PHPDocumentor'), ('Security'),('Design Patterns');
+INSERT INTO `Tag` (name) VALUES ('PHP'),('Quality Assurance'),('MySQL'),('php|tek'),('Arrays'),('PHPUnit'),('Refactoring'),('Tools'), ('Build Process'),('Deployment'), ('Integration'), ('DPC2012'),('PHPDocumentor'), ('Security'),('Design Patterns'),('PHPNW2011');
 
+
+/* Delete all Speaker records then re-load sample data */
+
+DELETE FROM `Speaker`;
+
+INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Cookie_Monster','cookie.png','Likes to eat biscuits. Chocolate chip cookies are his favourite');
+INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Ernie','ernie.png','Best friends with Bert.  They do everything together.');
+INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Fozzie','fozzie.png','Really friendly muppet.  Kids love him and he is so nice.');
+INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Gonzo','gonzo.png','Mischevious but loveable.  Is moody at times but generally good natured, if a little naughty.');
+
+
+/* Delete all Category records then re-load sample data */
+
+DELETE FROM `Category`;
+
+INSERT INTO `Category` (`name`) VALUES ('Tootie Frootie'),('Candi Bonbons'),('Toffee Apple'),('Rhubarb & Custard'),('Hard Boiled Sweets'),('Chewy Chews'),('Jam Donut');
