568c072110ba9e6e342a4f1664cfd9d0329a8b4d
Kim Rowan <rowan02@unknown-00-25-00-44-3a-04.home>
2012-04-15 11:02:36 +0100

    - have fixed my tabs (spaces)/ indent in IDE to 4 spaces, as requested.  Have not reformatted all pages though as wanted to check first whether we are putting first curly brace on same line as function name or the line beneath it.  Best to wait until we discuss it.
    - titles changed to title case in seed_data.sql. Amended main.css to transform to uppercase
    
    - TRUNCATE TABLE now used in seed_data.sql instead of DELETE FROM
    
    - main.css reformatted and white space removed, as requested.
    
    - changed favicon.ico image to lectern from logo :)

--- doc/db/seed_data.sql
+++ doc/db/seed_data.sql
@@ -8,23 +8,23 @@
 
 /* Delete all Page records then re-load sample data */
 
-DELETE FROM `Page`;
+TRUNCATE `Page`;
 
-INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contact','Contact Us','CONTACT','Some dummy text for contact page');
-INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contribute','How to Contribute','CONTRIBUTE','Some dummy text for contribute page');
-INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('about','About Us','ABOUT','Some dummy text for about page');
+INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contact','Contact Us','Contact','Some dummy text for contact page');
+INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('contribute','How to Contribute','Contribute','Some dummy text for contribute page');
+INSERT INTO `Page` (url,pagetitle,title,content) VALUES ('about','About Us','About','Some dummy text for about page');
 
 
 /* Delete all Tag records then re-load sample data */
 
-DELETE FROM `Tag`;
+TRUNCATE TABLE `Tag`;
 
 INSERT INTO `Tag` (name) VALUES ('PHP'),('Quality Assurance'),('MySQL'),('php|tek'),('Arrays'),('PHPUnit'),('Refactoring'),('Tools'), ('Build Process'),('Deployment'), ('Integration'), ('DPC2012'),('PHPDocumentor'), ('Security'),('Design Patterns'),('PHPNW2011');
 
 
 /* Delete all Speaker records then re-load sample data */
 
-DELETE FROM `Speaker`;
+TRUNCATE TABLE `Speaker`;
 
 INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Cookie_Monster','cookie.png','Likes to eat biscuits. Chocolate chip cookies are his favourite');
 INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Ernie','ernie.png','Best friends with Bert.  They do everything together.');
@@ -34,6 +34,6 @@ INSERT INTO `Speaker` (`name`, photo, biography ) VALUES ('Gonzo','gonzo.png','M
 
 /* Delete all Category records then re-load sample data */
 
-DELETE FROM `Category`;
+TRUNCATE TABLE `Category`;
 
 INSERT INTO `Category` (`name`) VALUES ('Tootie Frootie'),('Candi Bonbons'),('Toffee Apple'),('Rhubarb & Custard'),('Hard Boiled Sweets'),('Chewy Chews'),('Jam Donut');
