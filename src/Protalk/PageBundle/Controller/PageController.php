429f676adbdf8053e743ca1bfe10bebb1b44f285
Kim Rowan <rowan02@unknown-00-25-00-44-3a-04.home>
2012-04-29 11:26:52 +0100

    Removed dependency between PageBundle and Contributor entity in MediaBundle.  Contributor form now generated, persisted and displayed by ContributionController within MediaBundle

--- src/Protalk/PageBundle/Controller/PageController.php
+++ src/Protalk/PageBundle/Controller/PageController.php
@@ -2,8 +2,6 @@
 
 namespace Protalk\PageBundle\Controller;
 
-use Protalk\MediaBundle\Entity\Contribution;
-use Protalk\MediaBundle\Form\Media\ContributeMedia;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -22,32 +20,6 @@ class PageController extends Controller
         if (!is_object($page))
         throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException();
 
-        $contribution = new Contribution();
-        $form = $this->createForm(new ContributeMedia(), $contribution);
-
-        $request = $this->getRequest();
-
-        if ($request->getMethod() == 'POST') {
-            $form->bindRequest($request);
-
-            if ($form->isValid()) {
-                // perform some action, such as saving the task to the database
-
-                $contribution = $form->getData();
-                $em = $this->getDoctrine()->getEntityManager();
-                $em->persist($contribution);
-                $em->flush();
-
-                $this->get('session')->setFlash('contribution-notice', 'Thank you! Your contribution has been received.');
-
-                return $this->redirect($this->generateUrl('page_show', array('url' => 'contribute')));
-            }
-        }
-
-        return array(
-                'page' => $page,
-                'form' => $form->createView()
-                    );
-
+        return array('page' => $page);
     }
 }
