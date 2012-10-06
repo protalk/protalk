9e2c23036bd2fe7fe9ff63af28a35634b3925864
Lineke Kerckhoffs-Willems <lineke@Linekes-MacBook-Pro-2.local>
2012-10-06 15:14:23 +0200

    Removed the required option for the isPublished field

--- src/Protalk/AdminBundle/Admin/MediaAdmin.php
+++ src/Protalk/AdminBundle/Admin/MediaAdmin.php
@@ -24,7 +24,7 @@ class MediaAdmin extends Admin
                 ->add('slides')
                 ->add('joindin')
                 ->add('language')
-                ->add('isPublished')
+                ->add('isPublished', null, array('required' => false))
                 ->add('hostName')
                 ->add('hostUrl')
                 ->add('thumbnail')
