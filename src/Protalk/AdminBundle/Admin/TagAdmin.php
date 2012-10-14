f07fceb9583cdf63fbdf20c7743357638f052cb5
Steffan Harries <contact@steffanharries.me.uk>
2012-10-14 16:29:33 +0100

    Fixed bug #62 - creating a tag throws a 500 error

--- src/Protalk/AdminBundle/Admin/TagAdmin.php
+++ src/Protalk/AdminBundle/Admin/TagAdmin.php
@@ -21,21 +21,24 @@ class TagAdmin extends Admin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('name');
+        $formMapper->add('name')->add('slug');
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
-        $datagridMapper->add('name');
+        $datagridMapper->add('name')->add('slug');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('name');
+        $listMapper->addIdentifier('name')->add('slug');
     }
 
     public function validate(ErrorElement $errorElement, $object)
     {
-        $errorElement->with('name')->assertMaxLength(array('limit' => 50))->end();
+        $errorElement
+        ->with('name')
+        ->assertMaxLength(array('limit' => 50))
+        ->end();
     }
 }
