4c1d4823c1c963c26a25c4d544f901dc96b394f8
Lineke Kerckhoffs-Willems <lineke@phpassionate.com>
2012-10-18 21:26:35 +0200

    Added contribution admin class

---
+++ src/Protalk/AdminBundle/Admin/ContributionAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Protalk\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ContributionAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('category')
+                ->add('name')
+                ->add('email')
+                ->add('hostUrl')
+                ->add('hostName')
+                ->add('title')
+                ->add('slidesUrl')
+                ->add('speaker')
+                ->add('description')
+                ->add('tags');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name')->add('email')->add('title');
+    }
+}
