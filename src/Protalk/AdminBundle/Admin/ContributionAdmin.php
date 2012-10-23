6e289b8a64c0597ce228f669dbcf545689031421
Lineke Kerckhoffs-Willems <lineke@phpassionate.com>
2012-10-23 21:43:04 +0200

    Removed create button for contribution admin class

--- src/Protalk/AdminBundle/Admin/ContributionAdmin.php
+++ src/Protalk/AdminBundle/Admin/ContributionAdmin.php
@@ -7,9 +7,15 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
 
 class ContributionAdmin extends Admin
 {
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->remove('create');
+    }
+    
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper->add('category')
