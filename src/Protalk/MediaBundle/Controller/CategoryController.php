fb2060184c94d466f8e7a0e6d3f9506918f677a7
Kim Rowan <kim.rowan@cancer.org.uk>
2012-11-25 15:16:45 +0000

    Add truncate method to category controller to truncate category names and prevent line-wrap

--- src/Protalk/MediaBundle/Controller/CategoryController.php
+++ src/Protalk/MediaBundle/Controller/CategoryController.php
@@ -16,6 +16,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class CategoryController extends Controller
 {
+
     /**
      * @Template()
      */
@@ -25,6 +26,29 @@ class CategoryController extends Controller
         $repository = $em->getRepository('ProtalkMediaBundle:Category');
         $categories = $repository->getMostUsedCategories();
 
+        $categories = $this->truncateName($categories);
+
         return array('categories' => $categories);
+    }
+
+    /**
+     * Truncate name element in array of categories
+     *
+     * @param array $categories Array of categories
+     * @param integer $length Maximum allowed length of category name
+     * @return array
+     */
+    public function truncateName($categories, $length = 17)
+    {
+        $counter = 0;
+        foreach ($categories as $category) {
+            foreach ($category as $key => $value) {
+                if ($key == 'name') {
+                    $categories[$counter]['truncated'] = strlen($value) <= $length ? $value : substr($value, 0, $length) . '...';
+                }
+            }
+            $counter++;
+        }
+        return $categories;
     }
 }
