9c27b1d73a2060bcc431369da99b41cf82534949
Kim Rowan <kim.rowan@cancer.org.uk>
2012-11-30 13:36:54 +0000

    Remove truncate method and apply new twig truncate filter to template instead

--- src/Protalk/MediaBundle/Controller/CategoryController.php
+++ src/Protalk/MediaBundle/Controller/CategoryController.php
@@ -26,29 +26,6 @@ class CategoryController extends Controller
         $repository = $em->getRepository('ProtalkMediaBundle:Category');
         $categories = $repository->getMostUsedCategories();
 
-        $categories = $this->truncateName($categories);
-
         return array('categories' => $categories);
-    }
-
-    /**
-     * Truncate name element in array of categories
-     *
-     * @param array $categories Array of categories
-     * @param integer $length Maximum allowed length of category name
-     * @return array
-     */
-    public function truncateName($categories, $length = 17)
-    {
-        $counter = 0;
-        foreach ($categories as $category) {
-            foreach ($category as $key => $value) {
-                if ($key == 'name') {
-                    $categories[$counter]['truncated'] = strlen($value) <= $length ? $value : substr($value, 0, $length) . '...';
-                }
-            }
-            $counter++;
-        }
-        return $categories;
     }
 }
