0537a84ca71ea2278821bfe797ac1e17c22b48e1
Kim Rowan <kim.rowan@cancer.org.uk>
2013-02-08 13:37:02 +0000

    Fix typo in AppKernel introduced in recent PR

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -49,7 +49,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
