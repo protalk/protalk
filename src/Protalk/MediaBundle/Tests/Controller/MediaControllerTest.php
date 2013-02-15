8f8138bdba6eb2d0d967e976aa4d74085f4c3fbc
Steffan Harries <steffanharries@googlemail.com>
2013-02-15 00:02:49 +0000

    Adds simple tests for MediaController

---
+++ src/Protalk/MediaBundle/Tests/Controller/MediaControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * ProTalk
+ *
+ * Copyright (c) 2012-2013, ProTalk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ProTalk\PageBundle\Tests\Controller;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Protalk\MediaBundle\Controller\MediaController;
+
+class MediaControllerTest extends WebTestCase
+{
+    public function setUp()
+    {
+        $this->loadFixtures(
+            array(
+                'Protalk\MediaBundle\Tests\Fixtures\LoadMediaData'
+            )
+        );
+    }
+
+    public function testMediaPageShowsAllData()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/my-video-about-php');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        var_dump($response->getContent());
+        $this->assertContains("My video about PHP", $response->getContent());
+        $this->assertContains("PHPNW", $response->getContent());
+        $this->assertContains("Joe Bloggs", $response->getContent());
+        $this->assertContains("A video about PHP!", $response->getContent());
+        $this->assertContains("http://some.video-url.com", $response->getContent());
+        $this->assertContains("20:00:00", $response->getContent());
+    }
+
+    public function testMediaPageIncrementsMediaViewCount()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/my-video-about-php');
+        $response = $client->getResponse();
+
+        $this->assertContains("101 views", $response->getContent());
+
+        $client->request('GET', '/my-video-about-php');
+        $response = $client->getResponse();
+
+        $this->assertContains("102 views", $response->getContent());
+    }
+}
