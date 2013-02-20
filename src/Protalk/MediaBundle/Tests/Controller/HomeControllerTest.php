a30f2f0faeb9ec42ac0b65c937b4e378fb3c867b
Steffan Harries <steffanharries@googlemail.com>
2013-02-20 21:41:25 +0000

    Tests RSS feed shows valid info

---
+++ src/Protalk/MediaBundle/Tests/Controller/HomeControllerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * ProTalk
+ *
+ * Copyright (c) 2012-2013, ProTalk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ProTalk\PageBundle\Tests\Controller;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Protalk\MediaBundle\Controller\MediaController;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function setUp()
+    {
+        $this->loadFixtures(
+            array(
+                'Protalk\MediaBundle\Tests\Fixtures\LoadMediaData'
+            )
+        );
+    }
+
+
+    public function testGetFeedReturnsValidResponse()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/feed');
+
+        $response = $client->getResponse();
+        $content = $client->getResponse()->getContent();
+
+        // Assertions on the channel
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('<title>ProTalk RSS feed</title>', $content);
+        $this->assertContains('<atom:link href="http://protalk.me/feed/" rel="self" type="application/rss+xml" />', $content);
+        $this->assertContains('<link>http://protalk.me</link>', $content);
+
+        // Assertions on the items in the feed
+        $this->assertContains('<title>My video about PHP</title>', $content);
+        $this->assertContains('<link>http://protalk.me/my-video-about-php</link>', $content);
+
+        $today = new \DateTime();
+        $today = $today->format('d-m-Y');
+        
+        $this->assertContains('<pubDate>'.$today.'</pubDate>', $content);
+        $this->assertContains('<dc:creator>ProTalk</dc:creator>', $content);
+        $this->assertContains('<guid isPermaLink="false">http://protalk.me/my-video-about-php</guid>', $content);
+        $this->assertContains('<p>A video about PHP!</p>', $content);
+    }
+}
