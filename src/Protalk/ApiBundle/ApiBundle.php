4b13d206f489ebccc279cb03bdb1693b7f19049b
System Administrator <root@MacBook-Pro-van-Ron.local>
2013-06-06 22:19:40 +0200

    [FEATURE] Added Rest Bundle and setup basic API bundle

---
+++ src/Protalk/ApiBundle/ApiBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ProTalk\ApiBundle;
+
+use FOS\RestBundle\FOSRestBundle;
+
+class ApiBundle extends FOSRestBundle
+{
+
+}
---
+++ src/Protalk/ApiBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * ProTalk
+ *
+ * Copyright (c) 2012-2013, ProTalk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Protalk\ApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see
+ * {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('protalk_media');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+        return $treeBuilder;
+    }
+}
