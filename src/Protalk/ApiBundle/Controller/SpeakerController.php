4751733fb861cec0497000ee8c26dbeb40929b52
Ron van der Molen <ron@wizkunde.nl>
2013-06-07 00:46:34 +0200

    [FEATURE] Added the empty controllers and updated a minor DI setting

---
+++ src/Protalk/ApiBundle/Controller/SpeakerController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Protalk\ApiBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class SpeakerController extends FOSRestController
+{
+
+}
--- src/Protalk/ApiBundle/DependencyInjection/Configuration.php
+++ src/Protalk/ApiBundle/DependencyInjection/Configuration.php
@@ -28,7 +28,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('protalk_media');
+        $rootNode = $treeBuilder->root('protalk_api');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
