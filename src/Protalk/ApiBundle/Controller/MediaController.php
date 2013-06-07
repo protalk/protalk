e6a5a8147a1eeda7542115171f25f005de8153a4
Ron van der Molen <ron@wizkunde.nl>
2013-06-07 13:16:04 +0200

    [FEATURE] Added picking up all media items from the repository without filtering

--- src/Protalk/ApiBundle/Controller/MediaController.php
+++ src/Protalk/ApiBundle/Controller/MediaController.php
@@ -16,8 +16,11 @@ class MediaController extends FOSRestController
      * @Route("/media/{id}", name="api_media_show")
      * @Template()
      */
-    public function getMediaAction()
+    public function getMediaAction($id)
     {
+        $mediaRepository = $this->getDoctrine()->getRepository('ProtalkMediaBundle:Media');
+        $mediaCollection = $mediaRepository->findAll();
+
         return $this->render('ProtalkApiBundle:Media:getMedia.html.twig', array('media' => 'test'));
     }
 }
