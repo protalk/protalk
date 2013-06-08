e250831e6650fe89839583fcad69e5ed28cdd942
Ron van der Molen <ron@wizkunde.nl>
2013-06-08 09:41:23 +0200

    [FEATURE] Create HAL CMM compatible REST output

--- src/Protalk/ApiBundle/Controller/MediaListController.php
+++ src/Protalk/ApiBundle/Controller/MediaListController.php
@@ -8,6 +8,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\RestBundle\View\View;
 use Doctrine\ORM\Query;
+use JoshuaEstes\Hal\Link;
+use JoshuaEstes\Hal\Resource;
 
 /**
  * Class MediaController
@@ -21,15 +23,28 @@ class MediaListController extends FOSRestController
      */
     public function getMediaListAction()
     {
+        $resource = new Resource(new Link('/location', 'self'));
+
         $mediaRepository = $this->getDoctrine()->getRepository('ProtalkMediaBundle:Media');
         $mediaItems = $mediaRepository->getMediaOrderedBy('id', 1, 5, 'DESC', Query::HYDRATE_ARRAY);
 
-        $view = View::create(array('media' => $mediaItems['results']))
+        foreach($mediaItems['results'] as $item)
+        {
+            $mediaResource        = new Resource(new Link('/media/' . $item['id'], 'self'), 'media');
+            $mediaResource->title = 'bla';
+            $resource->addResource($mediaResource);
+        }
+
+        // You can add more links too
+        $resource->addLink(new Link('/location/next', 'next'));
+        $resource->addLink(new Link('/location/previous', 'previous'));
+
+        $view = View::create(array('media' => $resource->asArray()))
             ->setStatusCode(200)
             ->setEngine('twig')
             ->setTemplate('ProtalkApiBundle:Error:noHtml.html.twig')
             ->setTemplateVar('media')
-            ->setData($mediaItems['results']);
+            ->setData($resource->asArray());
 
         return $this->get('fos_rest.view_handler')->handle($view);
     }
