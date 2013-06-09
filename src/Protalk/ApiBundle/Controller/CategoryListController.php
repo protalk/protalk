cc7a89efd9d6b472821fea256c5b011563f77d5c
Ron van der Molen <ron@wizkunde.nl>
2013-06-09 15:26:14 +0200

    [FEATURE] Added Category List output at REST API

--- src/Protalk/ApiBundle/Controller/CategoryListController.php
+++ src/Protalk/ApiBundle/Controller/CategoryListController.php
@@ -21,16 +21,33 @@ class CategoryListController extends FOSRestController
      */
     public function getCategoryListAction()
     {
-        $mediaRepository = $this->getDoctrine()->getRepository('ProtalkMediaBundle:Media');
-        $mediaItems = $mediaRepository->findByCategory('php', 'id', 1, 5, 'DESC', Query::HYDRATE_ARRAY);
+        $categoryItems = $this->fetchCategoryItems();
+        $formattedCategoryItems = $this->container->get('protalk_api.helper.category_list')->buildArray($categoryItems);
 
-        $view = View::create(array('media' => $mediaItems['results']))
+        $view = View::create(array('media' => $formattedCategoryItems))
             ->setStatusCode(200)
             ->setEngine('twig')
             ->setTemplate('ProtalkApiBundle:Error:noHtml.html.twig')
             ->setTemplateVar('media')
-            ->setData($mediaItems['results']);
+            ->setData($formattedCategoryItems);
 
         return $this->get('fos_rest.view_handler')->handle($view);
+    }
+
+    /**
+     * Fetch the category items from the repository
+     *
+     * @return \RecursiveIteratorIterator
+     */
+    protected function fetchCategoryItems()
+    {
+        $countCategoryItems = $this->container->get('request')->get('count') ?: 10;
+        $pageCategoryItems = $this->container->get('request')->get('page') ?: 1;
+        $sortCategoryItems = $this->container->get('request')->get('sort') ?: 'DESC';
+
+        $categoryRepository = $this->getDoctrine()->getRepository('ProtalkMediaBundle:Category');
+        $categoryItems = $categoryRepository->getAllCategories(Query::HYDRATE_ARRAY);
+
+        return new \RecursiveArrayIterator($categoryItems);
     }
 }
