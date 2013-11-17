6c62c39c8a412900736802bcf0a08420194064d9
unknown <kim.rowan@cancer.org.uk>
2013-11-17 11:10:14 +0000

    Removing validate method on SpeakerAdmin class as it throws an error prevent addition of new speakers

--- src/Protalk/AdminBundle/Admin/SpeakerAdmin.php
+++ src/Protalk/AdminBundle/Admin/SpeakerAdmin.php
@@ -27,7 +27,6 @@ namespace Protalk\AdminBundle\Admin;
 use Sonata\AdminBundle\Admin\Admin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
-use Sonata\AdminBundle\Validator\ErrorElement;
 use Sonata\AdminBundle\Form\FormMapper;
 
 class SpeakerAdmin extends Admin
@@ -71,18 +70,5 @@ class SpeakerAdmin extends Admin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper->addIdentifier('name')->add('biography');
-    }
-
-    /**
-     * Validator function
-     *
-     * This function validates that name is no more tan 100 characters long.
-     *
-     * @param ErrorElement $errorElement
-     * @param mixed        $object
-     */
-    public function validate(ErrorElement $errorElement, $object)
-    {
-        $errorElement->with('name')->assertMaxLength(array('limit' => 100))->end();
     }
 }
