3f49154f31440d04158bff76a745be013943fb87
Kim Rowan <kim.rowan@cancer.org.uk>
2014-03-04 14:54:29 +0000

    Update puppet files to execute database migrations and load datafixtures

--- support/puppet/manifests/default.pp
+++ support/puppet/manifests/default.pp
@@ -304,7 +304,7 @@ if count($php_values['modules']['pecl']) > 0 {
   php_pecl_mod { $php_values['modules']['pecl']:; }
 }
 if count($php_values['ini']) > 0 {
-  $php_values['ini'].each { |$key, $value|
+  $php_values['ini'].each  |$key, $value| {
     puphpet::ini { $key:
       entry       => "CUSTOM/${key}",
       value       => $value,
@@ -375,7 +375,7 @@ if $xdebug_values['install'] != undef and $xdebug_values['install'] == 1 {
   }
 
   if is_hash($xdebug_values['settings']) and count($xdebug_values['settings']) > 0 {
-    $xdebug_values['settings'].each { |$key, $value|
+    $xdebug_values['settings'].each  |$key, $value| {
       puphpet::ini { $key:
         entry       => "XDEBUG/${key}",
         value       => $value,
