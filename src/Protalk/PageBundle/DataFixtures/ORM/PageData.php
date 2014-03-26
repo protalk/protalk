0c2ff7eb7665491cd7dfec8c9c4460e0de7b8967
Rafael Dohms <rdohms@gmail.com>
2014-03-26 22:25:37 +0100

    Data Fixtures
    - Users, Pages, Tags

---
+++ src/Protalk/PageBundle/DataFixtures/ORM/PageData.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Protalk\PageBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Protalk\PageBundle\Entity\Page;
+use Protalk\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class PageData extends AbstractFixture implements FixtureInterface, OrderedFixtureInterface
+{
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 10;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    function load(ObjectManager $manager)
+    {
+        $page = new Page();
+        $page->setUrl('contact');
+        $page->setPageTitle('Contact Us');
+        $page->setTitle('Contact');
+        $page->setContent("We'd really appreciate any feedback you have about the site. Bug reports are also very welcome. Go on, let us know what you think: </p><p>Email: <a href=\"mailto:info@protalk.me\">info@protalk.me</a><br>Twitter: <a href=\"http://twitter.com/pro_talk\">@pro_talk</a></p><p>You can also subscribe to our mailing list to keep in touch via our newsletter:<form action=\"http://protalk.us4.list-manage.com/subscribe/post?u=4192f6a464ac9b433d406efff&amp;id=e97aea0468\" method=\"post\" id=\"mc-embedded-subscribe-form\" name=\"mc-embedded-subscribe-form\" class=\"validate\">\r\n<input id=\"subscribeEmail\" class=\"subscribe\" name=\"EMAIL\" type=\"email\" placeholder=\"Enter your email address here\"/>\r\n<input id=\"subscribeButton\" class=\"subscribe\" type=\"image\" src=\"/images/subscribe_btn.png\" />\r\n</form>");
+        $manager->persist($page);
+
+        $this->addReference('page#contact', $page);
+
+        $page = new Page();
+        $page->setUrl('contribute');
+        $page->setPageTitle('How to Contribute');
+        $page->setTitle('Contribute');
+        $page->setContent("Contributing to ProTalk couldn\'t be easier. All you need to do is find a video or audio recording about PHP that you enjoyed and think others could learn from, complete the form below and we\'ll take it from there.</p><p>Once we\'ve verified that we\'re allowed to embed the material, we\'ll publish it on the site and send you an email letting you know its available. Well….what are you waiting for?</p><p>If you want to help us develop ProTalk and make it even better, <a class=\"pageLink\" href=\"https://github.com/protalk/protalk\">fork us on GitHub</a> and check out the <a class=\"pageLink\" href=\"https://github.com/protalk/protalk/issues\">list of open issues</a>.");
+        $manager->persist($page);
+
+        $this->addReference('page#contribute', $page);
+
+        $page = new Page();
+        $page->setUrl('about');
+        $page->setPageTitle('About Us');
+        $page->setTitle('About');
+        $page->setContent("ProTalk is the brain child of Kim Rowan and Lineke Kerckhoffs-Willems. A spark of an idea on IRC in July 2011 transformed into the site you see before you and we really hope you like it.</p><p>ProTalk\'s mission is to provide a central point of access to online audio / video content with a PHP focus. We hope to expand and include other programming languages in the future, but for now we\'re focussing solely on PHP and surrounding tools and skills.</p><p>ProTalk aims to be a community resource and, as such, we will depend on you to <a href=\"http://protalk.me/contribute/new\">contribute content</a>. Just send us links to recorded talks, postcasts, screencasts, etc., that are already online or hosted elsewhere and we\'ll do the rest.");
+        $manager->persist($page);
+
+        $this->addReference('page#about', $page);
+
+        $manager->flush();
+    }
+}
---
+++ src/Protalk/UserBundle/DataFixtures/ORM/UserData.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Protalk\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Protalk\UserBundle\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class UserData extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+{
+    const DEFAULT_PLAIN_PASSWORD = 'password';
+
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Sets the Container associated with this Controller.
+     *
+     * @param ContainerInterface $container A ContainerInterface instance
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 10;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    function load(ObjectManager $manager)
+    {
+        $factory = $this->container->get('security.encoder_factory');
+
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@protalk.me');
+        $user->setPlainPassword(self::DEFAULT_PLAIN_PASSWORD);
+        $password = $factory->getEncoder($user)->encodePassword(self::DEFAULT_PLAIN_PASSWORD, $user->getSalt());
+        $user->setPassword($password);
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_USER', 'ROLE_ADMIN'));
+        $user->setSuperAdmin(true);
+        $manager->persist($user);
+
+        $this->addReference('user#admin', $user);
+
+        $user = new User();
+        $user->setUsername('john');
+        $user->setEmail('john@protalk.me');
+        $user->setPlainPassword(self::DEFAULT_PLAIN_PASSWORD);
+        $password = $factory->getEncoder($user)->encodePassword(self::DEFAULT_PLAIN_PASSWORD, $user->getSalt());
+        $user->setPassword($password);
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_USER'));
+        $manager->persist($user);
+
+        $this->addReference('user#regular', $user);
+
+        $user = new User();
+        $user->setUsername('jane');
+        $user->setEmail('jane@protalk.me');
+        $user->setPlainPassword(self::DEFAULT_PLAIN_PASSWORD);
+        $password = $factory->getEncoder($user)->encodePassword(self::DEFAULT_PLAIN_PASSWORD, $user->getSalt());
+        $user->setPassword($password);
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_USER'));
+        $manager->persist($user);
+
+        $this->addReference('user#alternate', $user);
+
+        $manager->flush();
+    }
+}
